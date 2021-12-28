efa7c88665766f297d4cda0fcd15fce5693237b6
chewkameka <abhijithkumar2000@gmail.com>
2021-12-28 19:25:36 +0530

    added server

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ from app import databaseCreate
 databaseCreate.create()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080', debug=False)
+    from waitress import serve
+    serve(app,host='0.0.0.0', port=8080)
